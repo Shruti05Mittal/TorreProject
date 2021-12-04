0f30cb846dbcc52fb811e5a82c2b40001a9e375b
shruti mittal <shruti05.mittal@gmail.com>
2021-12-05 00:23:51 +0530

    Test Complete

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+namespace Torre.Pages
+{
+    public class IndexModel : PageModel
+    {
+        static HttpClient client = new HttpClient();
+        private string getUserDetailsByUserNamePath = "https://bio.torre.co/api/bios/shruti05mittal";
+        public Root UserInfo = null;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            UserInfo = new Root();
+        }
+
+        static Root GetRoot(string path)
+        {
+            Root root = null;
+            HttpResponseMessage response =  client.GetAsync(path).Result;
+            if (response.IsSuccessStatusCode)
+            {
+              //  root = response.Content.ReadAsStringAsync<Root>();
+            }
+            return root;
+        }
+        public void OnGet()
+        {
+            //var root = GetRoot(getUserDetailsByUserNamePath);
+            UserInfo = GetData().Result;
+        }
+        public async Task<Root> GetData()
+        {
+            Root userinformation = new Root();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(getUserDetailsByUserNamePath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    userinformation = JsonConvert.DeserializeObject<Root>(apiResponse);
+                }
+            }
+            return userinformation;
+        }
+    }
+}
---
+++ Pages/SkillDetail.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Torre.Pages
+{
+    public class SkillDetail: PageModel
+    {
+        public Strength strength { get; set; }
+        public string ErrorMessage { get; set; }
+        private string getUserDetailsByUserNamePath = "https://bio.torre.co/api/bios/shruti05mittal";
+        public void OnGet(string skillname)
+        {
+            
+            if (string.IsNullOrWhiteSpace(skillname))
+                ErrorMessage = "Skill name can't be blank";
+            var userdata = GetUserData();
+            if (userdata == null)
+                ErrorMessage = "No user found";
+            if (userdata.Result == null)
+                ErrorMessage = "No user found";
+            if (userdata.Result.strengths == null || userdata.Result.strengths.Count == 0)
+                ErrorMessage = "No skills found";
+            var selectedSkill = userdata.Result.strengths.Where(s => s.name.ToLower() == skillname.ToLower());
+            if (selectedSkill == null || selectedSkill.Count() == 0)
+                ErrorMessage = "Selected skill not found";
+            if (selectedSkill.Count() > 1)
+                ErrorMessage = "Selected skill exists multiple times";
+            if (string.IsNullOrWhiteSpace(ErrorMessage))
+                strength = selectedSkill.FirstOrDefault();
+        }
+        public async Task<Root> GetUserData()
+        {
+            Root userinformation = null;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(getUserDetailsByUserNamePath))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        userinformation = JsonConvert.DeserializeObject<Root>(apiResponse);
+                    }
+                }
+            }
+            return userinformation;
+        }
+    }
+}
